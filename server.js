74ba10e825a56ff15e66dd888ffe80a20fd65d4a
omerenma <godwin2341@gmail.com>
2021-02-03 10:03:27 +0100

    initial

--- server.js
+++ server.js
@@ -64,14 +64,14 @@ app.use("/api/user", userRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/role", roleRouter);
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "index.html"));
+//   });
+// }
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
