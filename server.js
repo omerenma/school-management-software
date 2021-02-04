5a4f66c45ee1d7559ee04232d8d221b839466fab
omerenma <godwin2341@gmail.com>
2021-02-04 15:42:13 +0100

    i

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "index.html"));
+    res.sendFile(path.resolve(__dirname + "client/public/index.html"));
   });
 }
 const port = process.env.PORT || 5000;
