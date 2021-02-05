fc060f0ffecbe23a9d0468cb9743df27bff81021
omerenma <godwin2341@gmail.com>
2021-02-05 14:01:41 +0100

    all

--- server.js
+++ server.js
@@ -69,7 +69,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.send("Hello");
+    const paths = path.resolve(__dirname + "/client/build/index.html");
+    res.sendFile(paths);
     //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
